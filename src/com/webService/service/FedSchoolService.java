0ceed92c3136e70b96069f30b90cefd82574ad55
Danial Zaki <5809913+dszaki@users.noreply.github.com>
2018-12-04 10:35:23 -0500

    Update FedSchoolService.java

--- src/com/webService/service/FedSchoolService.java
+++ src/com/webService/service/FedSchoolService.java
@@ -4,11 +4,24 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
-@Component
-public class FedSchoolService {
+@Service
+public class ReadFiles {
 
-    @PostConstruct
-    private void pullData(){
-
+    public ReadFiles(FedSchoolRepository fedSchoolRepository){
+     this.fedSchoolRepository = fedSchoolRepository;   
     }
+    
+    private void pullData(){
+    
+        StringReader reader = new StringReader(input);
+        CsvToBean<FedSchool> csv = new CsvToBeanBuilder<FedSchool>(reader)
+            .withType(FedSchool.class)
+            .withSkipLines(1)
+            .build();
+        FedSchoolRepository,saveAll(csv.parse());
+    }
+    public<T> void parseFile(String input, T cls){
+        if(cls.getClass().isInstance(FedSchool.class)) {
+           pullData(input);
+        }
 }
