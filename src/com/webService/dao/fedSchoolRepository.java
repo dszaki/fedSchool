373df208e9a0dd68107f1b96ccfaf87946c219a7
Danial Zaki <5809913+dszaki@users.noreply.github.com>
2018-12-04 10:29:15 -0500

    Update fedSchoolRepository.java

--- src/com/webService/dao/fedSchoolRepository.java
+++ src/com/webService/dao/fedSchoolRepository.java
@@ -3,7 +3,7 @@ package com.webService.dao;
 import com.webService.domain.fedSchool;
 import org.springframework.data.repository.CrudRepository;
 
-public interface fedSchoolRepository extends CrudRepository<fedSchool, Long> {
+public interface FedSchoolRepository extends CrudRepository<fedSchool, Long> {
 
 }
 
